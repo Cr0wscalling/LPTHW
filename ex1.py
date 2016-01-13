e14077f6c5be37660968028c344e15ede9f96663
Cr0wscalling <gitcr0wscalling@gmail.com>
2016-01-12 19:54:21 -0800

    REMOVE old file

--- ex1.py
+++
@@ -1,7 +0,0 @@
-print "Hello World!"
-print "Hello Again"
-#print 'Yay! Printing.'
-#print "I'd much rather you 'not'."
-#print 'I "said"python do not touch this.'
-#printing a octothrope before a line mutes it. Placeing it before the text to be printed dose 
-#not mute the line from being printed you have to place it before the print function to mute line printin

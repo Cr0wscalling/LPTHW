e64cc409cfe2ba80d730a80c039990bc1041cb80
Cr0wscalling <gitcr0wscalling@gmail.com>
2016-01-12 20:04:43 -0800

    ADD exercise 3a

---
+++ LPTHWex3a.py
@@ -0,0 +1,28 @@
+# Exercise 3a gives instruction on mathematical operations, order of operation, and the modulus
+print "I will now count my chickens:"
+#initial exercise concantenating string with int types
+print "Hens", 25 + 30 / 6
+# first useage of modulus
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+#demonstrates order of operations (PEMDAS)
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Boolean logic test, but how, data appears to be type string
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+# or is it the return of line fourteen, lets add a new line and find out (see line 26)
+# Break down of operations
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater", 5 > - 2
+print "Is it geater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print "Lets see if the new line worked as expected"
+print 5 + 7 != 6 + 4 
